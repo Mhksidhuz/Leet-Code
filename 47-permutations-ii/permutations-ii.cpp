cd0b2b8e08f7f23f58a8384cefa3c0f283d829eb
Mhksidhuz <sidhumehak765@gmail.com>
2026-08-20 23:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (31.80%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-
-    void permutation(int index, vector<int> &nums, vector<vector<int>> &temp){
-        if(index==nums.size()){
-            temp.push_back(nums);
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
             return;
         }
-        unordered_set<int> used;
-
-        for(int i=index;i<nums.size();i++){
-            if(used.count(nums[i]))
-                continue;
-
-            used.insert(nums[i]);
-            swap(nums[i],nums[index]);
-            permutation(index+1,nums,temp);
-            swap(nums[i],nums[index]);
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
         }
     }
-
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> temp;
-        permutation(0,nums,temp);
-        return temp;
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
