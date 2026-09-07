ca55709b55661ec0c775dc3d7fae9c3f17187400
Mhksidhuz <sidhumehak765@gmail.com>
2026-09-07 20:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (52.62%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        
+        vector<int> ans;
+
+        for(auto it : nums){
+            if(it%2==0){
+                ans.insert(ans.begin(),it);
+            }
+            else{
+                ans.push_back(it);
+            }
+        }
+        return ans;
+    }
+};
