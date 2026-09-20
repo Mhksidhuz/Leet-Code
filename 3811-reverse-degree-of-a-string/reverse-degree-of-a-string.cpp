266c86ba85c513d4ce3a9fbb25cfe9a0e34cbb00
Mhksidhuz <sidhumehak765@gmail.com>
2026-09-20 15:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int reverseDegree(string s) {
         
-        int ans = 1;
+        int ans = 0;
 
         for(int i=0;i<s.size();i++){
             int val = 0;
             val = 26-(s[i]-'a');
             ans+=(val*(i+1));
         }
-        return ans-1;
+        return ans;
     }
 };
